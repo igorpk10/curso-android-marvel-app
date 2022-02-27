9852b6a850c0fb6b127b553fde2f359e34bca783
Igor Goncalves <igorgonsalves96@hotmail.com>
2022-02-27 20:29:24 -0300

    Setting navigation configs

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.*
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+    }
+
+    private var navController: NavController? = null
+    private var appBarConfiguration: AppBarConfiguration? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(setOf())
     }
 }
