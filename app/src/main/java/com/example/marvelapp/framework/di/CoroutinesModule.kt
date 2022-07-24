a1f0b0920e703c10a207aaf38ceaee8ec56a03a6
Igor Goncalves <igorgonsalves96@hotmail.com>
2022-07-24 17:25:59 -0300

    Implementing DetailViewModel Tests

--- app/src/main/java/com/example/marvelapp/framework/di/CoroutinesModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/CoroutinesModule.kt
@@ -2,6 +2,7 @@ package com.example.marvelapp.framework.di
 
 import com.igaopk10.core.usecase.base.AppCoroutinesDispatcher
 import com.igaopk10.core.usecase.base.CoroutinesDispatchers
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,6 +12,6 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 interface CoroutinesModule {
 
-    @Provides
+    @Binds
     fun bindDispatchers(dispatcher: AppCoroutinesDispatcher): CoroutinesDispatchers
 }
